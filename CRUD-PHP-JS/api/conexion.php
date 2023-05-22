39570cc65b42312db08b009ccbcd6efcf24bd8cc
xDhaz95 <134274928+xDhaz95@users.noreply.github.com>
2023-05-22 12:00:53 +0200

    proyecto subido para railway - daw

---
+++ CRUD-PHP-JS/api/conexion.php
@@ -0,0 +1,11 @@
+<?php
+    
+    // Variables de la conexion a la DB
+    $DB_HOST = $_ENV["DB_HOST"];
+    $DB_USER = $_ENV["DB_USER"];
+    $DB_PASSWORD=$_ENV["DB_PASSWORD"];
+    $DB_NAME=$_ENV["DB_NAME"];
+    $DB_PORT=$_ENV["DB_PORT"];
+    $db=mysqli_connect("$DB_HOST", "$DB_USER", "$DB_PASSWORD", "$DB_NAME", "$DB_PORT");
+    
+?>
